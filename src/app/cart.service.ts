c47b711cf17f0ee41a8bde183826ee300e24d34e
maxiinsfran <maxiinsfran@gmail.com>
2023-12-13 17:24:37 -0300

    adding buttons and form for buy

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,11 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
   items: Product[] = [];
+
+  constructor(
+    private http: HttpClient
+  ) { }
 
   addToCart(product: Product) {
     this.items.push(product);
@@ -20,5 +26,8 @@ export class CartService {
     return this.items;
   }
 
-  constructor() { }
+  getShippingPrices(): Observable<{ type: string; price: number; }[]> {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
+  }
+
 }
